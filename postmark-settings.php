05c898c554ad5a315153e4e500e55f164593ed44
Patrick Graham <patrick@wildbit.com>
2021-07-20 14:11:01 -0700

    Style adherence.

--- postmark-settings.php
+++ postmark-settings.php
@@ -7,11 +7,11 @@
 
 $settings = json_decode( get_option( 'postmark_settings' ), true );
 $settings_from_constants = array(
-    'api_key'        => defined('POSTMARK_API_KEY') ? POSTMARK_API_KEY : null,
-    'stream_name'    => defined('POSTMARK_STREAM_NAME') ? POSTMARK_STREAM_NAME : null,
-    'sender_address' => defined('POSTMARK_SENDER_ADDRESS') ? POSTMARK_SENDER_ADDRESS : null,
+    'api_key'        => defined( 'POSTMARK_API_KEY' ) ? POSTMARK_API_KEY : null,
+    'stream_name'    => defined( 'POSTMARK_STREAM_NAME' ) ? POSTMARK_STREAM_NAME : null,
+    'sender_address' => defined( 'POSTMARK_SENDER_ADDRESS' ) ? POSTMARK_SENDER_ADDRESS : null,
 );
-$settings_from_constants = array_filter($settings_from_constants);
+$settings_from_constants = array_filter( $settings_from_constants );
 $settings = array_merge( $settings, $settings_from_constants );
 return array(
     'settings' => $settings,
