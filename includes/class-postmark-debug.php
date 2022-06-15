9d47610c82b454658cd2fba1777ec955c8bdfe72
Patrick Graham <patrick@wildbit.com>
2022-06-15 15:33:12 -0400

    PHPCBF cleanup.

--- includes/class-postmark-debug.php
+++ includes/class-postmark-debug.php
@@ -1,105 +1,107 @@
 <?php
 
-if( ! class_exists( 'Postmark_Debug' ) ) {
+if ( ! class_exists( 'Postmark_Debug' ) ) {
 
-  class Postmark_Debug {
+	class Postmark_Debug {
 
-    /**
-     * Constructor.
-     */
-    public function __construct() {
-      // https://make.wordpress.org/core/2019/04/25/site-health-check-in-5-2/
-      add_filter( 'debug_information', [ $this, 'debug_info' ] );
-    }
+		/**
+		 * Constructor.
+		 */
+		public function __construct() {
+			// https://make.wordpress.org/core/2019/04/25/site-health-check-in-5-2/
+			add_filter( 'debug_information', array( $this, 'debug_info' ) );
+		}
 
-    /**
-     * WordPress Site Health Debug Info.
-     * @param  [type] $debug_info Debug Info.
-     */
-    public function debug_info( $debug_info ) {
+		/**
+		 * WordPress Site Health Debug Info.
+		 *
+		 * @param  [type] $debug_info Debug Info.
+		 */
+		public function debug_info( $debug_info ) {
 
-    	$settings = json_decode( get_option( 'postmark_settings' ) );
+			$settings = json_decode( get_option( 'postmark_settings' ) );
 
-        $debug_info['postmark-wordpress'] = array(
-            'label'    => __( 'Active Campaign Postmark', 'postmark-wordpress' ),
-            'fields'   => array(
-                'status' => array(
-                    'label'    => __( 'Enabled', 'postmark-wordpress' ),
-                    'value'   =>  $this->strbool( $settings->enabled ),
-                    'private' => false,
-                ),
-    						'version' => array(
-                    'label'    => __( 'POSTMARK_VERSION', 'postmark-wordpress' ),
-                    'value'   => POSTMARK_VERSION,
-                    'private' => false,
-                ),
-    						'directory' => array(
-                    'label'    => __( 'POSTMARK_DIR', 'postmark-wordpress' ),
-                    'value'   => POSTMARK_DIR,
-                    'private' => false,
-                ),
-    						'url' => array(
-                    'label'    => __( 'POSTMARK_URL', 'postmark-wordpress' ),
-                    'value'   => POSTMARK_URL,
-                    'private' => false,
-                ),
-    						'apikey' => array(
-                    'label'    => __( 'API Key', 'postmark-wordpress' ),
-                    'value'   => $settings->api_key,
-                    'private' => true,
-                ),
-    						'stream' => array(
-                    'label'    => __( 'Stream', 'postmark-wordpress' ),
-                    'value'   => $settings->stream_name,
-                    'private' => false,
-                ),
-    						'sender_address' => array(
-                    'label'    => __( 'Sender Address', 'postmark-wordpress' ),
-                    'value'   => $settings->sender_address,
-                    'private' => false,
-                ),
-    						'force_from' => array(
-                    'label'    => __( 'Force From', 'postmark-wordpress' ),
-                    'value'   => $this->strbool( $settings->force_from ),
-                    'private' => false,
-                ),
-    						'force_html' => array(
-                    'label'    => __( 'Force HTML', 'postmark-wordpress' ),
-                    'value'   => $this->strbool( $settings->force_html ),
-                    'private' => false,
-                ),
-    						'track_opens' => array(
-                    'label'    => __( 'Track Opens', 'postmark-wordpress' ),
-                    'value'   => $this->strbool( $settings->track_opens ),
-                    'private' => false,
-                ),
-    						'track_links' => array(
-                    'label'    => __( 'Track Links', 'postmark-wordpress' ),
-                    'value'   => $this->strbool( $settings->track_links ),
-                    'private' => false,
-                ),
-    						'enable_logs' => array(
-                    'label'    => __( 'Logs Enabled', 'postmark-wordpress' ),
-                    'value'   => $this->strbool( $settings->enable_logs ),
-                    'private' => false,
-                ),
-            ),
-        );
+			$debug_info['postmark-wordpress'] = array(
+				'label'  => __( 'Active Campaign Postmark', 'postmark-wordpress' ),
+				'fields' => array(
+					'status'         => array(
+						'label'   => __( 'Enabled', 'postmark-wordpress' ),
+						'value'   => $this->strbool( $settings->enabled ),
+						'private' => false,
+					),
+					'version'        => array(
+						'label'   => __( 'POSTMARK_VERSION', 'postmark-wordpress' ),
+						'value'   => POSTMARK_VERSION,
+						'private' => false,
+					),
+					'directory'      => array(
+						'label'   => __( 'POSTMARK_DIR', 'postmark-wordpress' ),
+						'value'   => POSTMARK_DIR,
+						'private' => false,
+					),
+					'url'            => array(
+						'label'   => __( 'POSTMARK_URL', 'postmark-wordpress' ),
+						'value'   => POSTMARK_URL,
+						'private' => false,
+					),
+					'apikey'         => array(
+						'label'   => __( 'API Key', 'postmark-wordpress' ),
+						'value'   => $settings->api_key,
+						'private' => true,
+					),
+					'stream'         => array(
+						'label'   => __( 'Stream', 'postmark-wordpress' ),
+						'value'   => $settings->stream_name,
+						'private' => false,
+					),
+					'sender_address' => array(
+						'label'   => __( 'Sender Address', 'postmark-wordpress' ),
+						'value'   => $settings->sender_address,
+						'private' => false,
+					),
+					'force_from'     => array(
+						'label'   => __( 'Force From', 'postmark-wordpress' ),
+						'value'   => $this->strbool( $settings->force_from ),
+						'private' => false,
+					),
+					'force_html'     => array(
+						'label'   => __( 'Force HTML', 'postmark-wordpress' ),
+						'value'   => $this->strbool( $settings->force_html ),
+						'private' => false,
+					),
+					'track_opens'    => array(
+						'label'   => __( 'Track Opens', 'postmark-wordpress' ),
+						'value'   => $this->strbool( $settings->track_opens ),
+						'private' => false,
+					),
+					'track_links'    => array(
+						'label'   => __( 'Track Links', 'postmark-wordpress' ),
+						'value'   => $this->strbool( $settings->track_links ),
+						'private' => false,
+					),
+					'enable_logs'    => array(
+						'label'   => __( 'Logs Enabled', 'postmark-wordpress' ),
+						'value'   => $this->strbool( $settings->enable_logs ),
+						'private' => false,
+					),
+				),
+			);
 
-        return $debug_info;
-    }
+			return $debug_info;
+		}
 
-    /**
-     * String Boolean.
-     * @param  bool   $value  Value.
-     * @return string Yes or No based on boolean provided.
-     */
-    private function strbool( bool $value ) {
-      return $value ? 'Yes' : 'No';
-    }
+		/**
+		 * String Boolean.
+		 *
+		 * @param  bool $value  Value.
+		 * @return string Yes or No based on boolean provided.
+		 */
+		private function strbool( bool $value ) {
+			return $value ? 'Yes' : 'No';
+		}
 
-  }
+	}
 
-  new Postmark_Debug;
+	new Postmark_Debug();
 
 }
